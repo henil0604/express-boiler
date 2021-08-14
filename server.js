3d91f746a1fdfc5c341b02b057ef181ba356cf08
henil0604 <henilmalaviya06@gmail.com>
2021-08-14 16:49:25 +0530

    PORT Error Fixed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ log("Loading Modules...");
 let app = require('./app');
 const env = require("./app/helpers/env");
 const RouterManager = require("./app/middlewares/RouterManager")
+const PORT = env("PORT") || 4141;
 
 app.use(require("./app/middlewares/hit"));
 
@@ -13,7 +14,7 @@ app = RouterManager(
 
 
 
-app.listen(env("PORT"), () => {
-    log(`Listening on PORT ${log.chalk.greenBright(env("PORT"))}`, 'success');
+app.listen(PORT, () => {
+    log(`Listening on PORT ${log.chalk.greenBright(PORT)}`, 'success');
     log(`Load time: ${Date.now() - app.__STARTING_TIME}ms`);
 });
