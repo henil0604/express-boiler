8fb80a245d857ec98661ea8e56f86ac5fee5d330
henil0604 <henilmalaviya06@gmail.com>
2021-08-21 22:42:58 +0530

    bodyParser and cookieParser support added

--- app/index.js
+++ app/index.js
@@ -1,7 +1,16 @@
 const STARTING_TIME = Date.now();
 const express = require('express');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 let app = express();
+
+// Applying bodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Applying cookieParser
+app.use(cookieParser())
 
 app.__proto__.__STARTING_TIME = STARTING_TIME;
 app.__proto__.express = express;
