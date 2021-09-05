c8b5a3663a3e0e0eca144aed8231591f7d083be0
Malaviya Henil <henilmalaviya06@gmail.com>
2021-09-05 18:33:14 +0530

    Removed req.db
    Removed req.db as it is not needed for every request and was causing the server to be slow

--- app/middlewares/RequestParser.js
+++ app/middlewares/RequestParser.js
@@ -4,7 +4,6 @@ const createResponse = require("../helpers/createResponse");
 
 module.exports = async (req, res, next) => {
 
-    req.db = await mongo();
     req.createResponse = createResponse;
     req.resolve = resolveRequest(req, res)
 
