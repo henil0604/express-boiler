4728a215b56bb4a1cd83e5ab80e5e7d1f30dc752
henil0604 <henilmalaviya06@gmail.com>
2021-10-24 14:33:40 +0530

    implemented new middlewares and socket server initialization

--- server.js
+++ server.js
@@ -5,26 +5,41 @@ const env = require("./app/helpers/env");
 const RouterManager = require("./app/middlewares/RouterManager")
 const PORT = env("PORT") || 4141;
 const path = require("path");
+const requestIp = require('request-ip');
+const useragent = require('express-useragent');
+
+// Initilizing MongoDb Connection
+require("./app/helpers/InitializeMongoDb")()
 
 app.use(require("./app/middlewares/hit"));
-app.use(require("./app/middlewares/RequestParser"));
+
+app.use(useragent.express())
+app.use(requestIp.mw())
 
 // Setting Globals
 globalThis.env = env;
 globalThis.app = app;
 globalThis.log = log;
+globalThis.helperJs = require("@henil0604/helperjs");
 globalThis.imp = (d) => {
     const p = `${path.join(__dirname, d)}`
     return require(p);
 }
+
+app.use(require("./app/middlewares/RequestParser"));
 
 app = RouterManager(
     app,
     require("./app/data/routes")
 )
 
+// Preparing Socket Server
+const { server, io } = require("./app/helpers/SocketIo.js")(app)
 
-app.listen(PORT, () => {
+globalThis.server = server;
+globalThis.io = io;
+
+server.listen(PORT, () => {
     log(`Listening on PORT {${PORT}}`, 'success');
     log(`Load time: {${Date.now() - app.__STARTING_TIME}ms}`);
 });
