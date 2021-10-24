02ad1c973e19a08f149cd1baa6b4c0edb85987c3
henil0604 <henilmalaviya06@gmail.com>
2021-10-24 14:35:37 +0530

    list of Response Codes. Will be provided at req.code

---
+++ app/data/responseCodes.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "SUCCESS": "SUCCESS",
+    "CREATED": "CREATED",
+    "OK": "OK",
+    "DONE": "DONE",
+    "PROVIDED": "PROVIDED",
+    "FOUND": "FOUND",
+    "FAILED": "FAILED",
+    "ERROR": "ERROR",
+    "SERVER_ERROR": "SERVER_ERROR"
+}
