a2f6dcb1bc61c1e2c94bb904b09d54ce1beed330
henil0604 <henilmalaviya06@gmail.com>
2021-10-24 14:36:20 +0530

    added `req.code`

--- app/middlewares/RequestParser.js
+++ app/middlewares/RequestParser.js
@@ -1,11 +1,12 @@
-const mongo = require("../helpers/mongo");
-const resolveRequest = require("../helpers/resolveRequest");
-const createResponse = require("../helpers/createResponse");
+const resolveRequest = imp("app/helpers/resolveRequest");
+const createResponse = imp("app/helpers/createResponse");
+const responseCodes = imp("app/data/responseCodes");
 
 module.exports = async (req, res, next) => {
 
     req.createResponse = createResponse;
     req.resolve = resolveRequest(req, res)
+    req.code = responseCodes;
 
     next()
 }
