0f6164ba52f59167d5c22354663d1f080c25aab7
henil0604 <henilmalaviya06@gmail.com>
2021-12-15 11:03:48 +0530

    Bug Fixed

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const log = require("./app/helpers/log");
+const log = require("@helper-modules/log");
 log("Loading Modules...");
 let app = require('./app');
-const env = require("./app/helpers/env");
+const env = require("@helper-modules/env");
 const RouterManager = require("./app/middlewares/RouterManager")
 const PORT = env("PORT") || 4141;
 const path = require("path");
 const requestIp = require('request-ip');
 const useragent = require('express-useragent');
+
+// Setting Globals
+globalThis.app = app;
+globalThis.env = env;
+globalThis.log = log;
+globalThis.helperJs = require("@henil0604/helperjs");
+globalThis.imp = (d) => {
+    const p = `${path.join(__dirname, d)}`
+    return require(p);
+}
 
 // Initilizing MongoDb Connection
 require("./app/helpers/InitializeMongoDb")()
@@ -15,16 +25,6 @@ app.use(require("./app/middlewares/hit"));
 
 app.use(useragent.express())
 app.use(requestIp.mw())
-
-// Setting Globals
-globalThis.app = app;
-globalThis.env = require("@helper-modules/env");
-globalThis.log = require("@helper-modules/log");
-globalThis.helperJs = require("@henil0604/helperjs");
-globalThis.imp = (d) => {
-    const p = `${path.join(__dirname, d)}`
-    return require(p);
-}
 
 app.use(require("./app/middlewares/RequestParser"));
 
